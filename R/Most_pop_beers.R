e6252c8574be37091ce4d94f4ce18a7674950eff
jaredgd <84406414+jaredgd@users.noreply.github.com>
2024-03-26 13:26:34 -0400

    Adding documentation files for add'l dataframes

---
+++ R/Most_pop_beers.R
@@ -0,0 +1,26 @@
+#' Beers from beeradvocate.com
+#'
+#' This package contains beer ratings and metadata for the top rated beers on beeradvocate.com
+#' @docType package
+#' @name CraftCans
+#' @aliases CraftCans CraftCans-package
+NULL
+
+#' "CraftCans"
+#'
+#' A data set containing golf discs approved by
+#' the Professional Disc Golf Association
+#' as of October, 2020
+#'
+#' @source \url{https://www.beeradvocate.com}
+#' @format A dataframe with 250 elements
+#' \describe{
+#'   \item{rank}{Ranking based on user ratings}
+#'   \item{name}{Name of beer}
+#'   \item{brewery}{Brewery where beer was brewed}
+#'   \item{type}{Style/type}
+#'   \item{ABV}{ABV of the beer}
+#'   \item{num_ratings}{Number of user ratings submitted}
+#'   \item{avg_rating}{Average user rating}
+#' }
+"CraftCans"
